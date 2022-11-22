4d90159927d724f069f811a2ac5b02f37a3eb43d
Elisey Esaulkov <toelisey@yandex.ru>
2022-11-22 16:24:07 +0500

    feat: add initial elements(cards) in the layout by JS

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,37 @@ const editFormSubmitButtonElement = editFormElement.querySelector('.profile-edit
 let editFormInputFieldName = editFormElement.querySelector('.profile-edit-form__input_kind_name');
 let editFormInputFieldDescription = editFormElement.querySelector('.profile-edit-form__input_kind_description');
 
+//elements variables
+const elementsElement = pageElement.querySelector('.elements');
+const elementsListElement = pageElement.querySelector('.elements__list');
+
+const initialElements = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 //
 // functions
 
@@ -42,6 +73,27 @@ function editFormSubmitHandler (evt) {
       closeEditForm();
     }
 }
+
+//elements's functions
+const addElement = function(nameValue, linkValue) {
+  const elementTemplate = pageElement.querySelector('#element').content;
+  const elementElement = elementTemplate.querySelector('.elements__element').cloneNode(true);
+  const imgElement = elementElement.querySelector('img');
+
+  elementElement.querySelector('.elements__description').textContent = nameValue;
+  imgElement.alt = nameValue;
+  imgElement.src = linkValue;
+
+  elementsListElement.append(elementElement);
+}
+
+const addInitialElements = function(initialElements) {
+  initialElements.forEach(element => {
+    addElement(element.name, element.link);
+  });
+}
+
+addInitialElements(initialElements);
 
 // listeners
 editFormOpenButtonElement.addEventListener('click', openEditForm);
